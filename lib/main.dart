731426795f1e8dc29fc50c5ff13354b700f6dcf0
Tobias Pritsch <tobias.pritsch@cosee.biz>
2019-05-23 13:00:29 +0200

    The BLoC is now used by our Apps StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,41 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:rxdart/rxdart.dart';
+import 'bloc.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+//Converted the StatelessWidget to a StatefulWidget.
+//Since our BLoC is supposed to represent our State,
+// in most cases the BLoC should be placed inside a StatefulWidgets State
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Here comes our first BLoC!
+  Bloc bloc = Bloc();
+
+  // The rest hasn't changed much.
   @override
   Widget build(BuildContext context) {
-    BehaviorSubject<String> behaviorSubject =
-        BehaviorSubject<String>.seeded('Loading');
-    //Using RxDarts BehaviorSubject allows us to easily access the last
-    //emitted value.
-    //Also we can subscribe multiple times.
-
-    behaviorSubject.stream.listen(print);
-    //This is equivalent to
-    //  controller.stream.listen((String msg) => print(msg));
-
-    behaviorSubject.stream.listen(printSpecial);
-    //Oh yes! Now we don't crash anymore!
-
-    //Now we add the newest state later than the creation of the StreamBuilder.
-    //So finally our values get processed by it.
-    for (int i = 1; i <= 10; i++) {
-      Timer(Duration(seconds: i), () {
-        behaviorSubject.sink.add('$i');
-      });
-    }
-
-    Timer(Duration(seconds: 11), () {
-      behaviorSubject.close();
-    });
-
-    //We will probably never see anything but the 'Loading' text :(
     return MaterialApp(
       builder: (context, widget) => Scaffold(
             body: Center(
-              //We use this StreamBuilder to consume a stream.
-              //Everytime the stream emits a new state, the builder will be triggered again.
               child: StreamBuilder(
-                stream: behaviorSubject.stream,
-
-                //Accessing the last emitted value:
-                initialData: behaviorSubject.value,
+                initialData: bloc.initialState(),
+                stream: bloc.state,
                 builder: (context, AsyncSnapshot<String> snapshot) {
-                  return snapshot.hasData
-                      ? Text(snapshot.data)
-                      : Text('Loading');
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(snapshot.data, textScaleFactor: 4),
+                      RaisedButton(
+                        child: Text('Count!'),
+                        onPressed: () => bloc.countUp.add(null),
+                      )
+                    ],
+                  );
                 },
               ),
             ),
@@ -55,5 +43,4 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  printSpecial(String input) => print('*** $input ***');
 }
