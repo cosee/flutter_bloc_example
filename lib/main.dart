3c298813b63abb2eb189a52fc1e7de1f938051cc
Tobias Pritsch <tobias.pritsch@cosee.biz>
2019-05-23 12:27:42 +0200

    Used RxDarts BehaviourSubject for providing the StreamBuilders initialValue

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,37 @@
 import 'package:flutter/material.dart';
-
 import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    BehaviorSubject<String> behaviorSubject =
+        BehaviorSubject<String>.seeded('Loading');
+    //Using RxDarts BehaviorSubject allows us to easily access the last
+    //emitted value.
+    //Also we can subscribe multiple times.
 
-    //Don't think Java 8+ Streams. Think Rx/Reactive.
-    StreamController<String> controller = StreamController<String>.broadcast();
-    //Using the .broadcast() constructor lets us register more than 1 listener!
-
-    controller.stream.listen(print);
+    behaviorSubject.stream.listen(print);
     //This is equivalent to
     //  controller.stream.listen((String msg) => print(msg));
 
-
-    controller.stream.listen(printSpecial);
+    behaviorSubject.stream.listen(printSpecial);
     //Oh yes! Now we don't crash anymore!
-
 
     //Now we add the newest state later than the creation of the StreamBuilder.
     //So finally our values get processed by it.
-    for (int i = 0; i <= 10; i++) {
+    for (int i = 1; i <= 10; i++) {
       Timer(Duration(seconds: i), () {
-        controller.sink.add('$i');
+        behaviorSubject.sink.add('$i');
       });
     }
 
     Timer(Duration(seconds: 11), () {
-      controller.close();
+      behaviorSubject.close();
     });
-
 
     //We will probably never see anything but the 'Loading' text :(
     return MaterialApp(
@@ -41,7 +40,10 @@ class MyApp extends StatelessWidget {
               //We use this StreamBuilder to consume a stream.
               //Everytime the stream emits a new state, the builder will be triggered again.
               child: StreamBuilder(
-                stream: controller.stream,
+                stream: behaviorSubject.stream,
+
+                //Accessing the last emitted value:
+                initialData: behaviorSubject.value,
                 builder: (context, AsyncSnapshot<String> snapshot) {
                   return snapshot.hasData
                       ? Text(snapshot.data)
